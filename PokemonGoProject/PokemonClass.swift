8f7afd33dfcf79aecbf741d991e7b73da4de10cf
Sam Yeh <syeh5413@stu.d214.org>
2022-04-26 14:21:16 -0500

    added properties to class for pokemon

--- PokemonGoProject/PokemonClass.swift
+++ PokemonGoProject/PokemonClass.swift
@@ -7,3 +7,16 @@
 
 import Foundation
 
+class Pokemon {
+    
+    var numberID = ""
+    var name = ""
+    var category = ""
+    var height = ""
+    var weight = ""
+    var description = ""
+    var pictureName = ""
+    var weakness = ""
+    var type = ""
+    var type2 = ""
+}
