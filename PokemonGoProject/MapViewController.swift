df2e648476cc217abf9773c4db5abc288a3ce147
Sam Yeh <syeh5413@stu.d214.org>
2022-04-28 14:31:02 -0500

    finished code for zooming and showing gyms

--- PokemonGoProject/MapViewController.swift
+++ PokemonGoProject/MapViewController.swift
@@ -28,8 +28,45 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
+        
     }
     
-
-
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        if isInitialLoad {
+            initialRegion = MKCoordinateRegion(center: mapView.centerCoordinate, span: mapView.region.span)
+            isInitialLoad = false
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+    }
+    
+    @IBAction func whenZoomButtonPressed(_ sender: Any) {
+        let myCenter = currentLocation.coordinate
+        let mySpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let myRegion = MKCoordinateRegion(center: myCenter, span: mySpan)
+        mapView.setRegion(myRegion, animated: true)
+    }
+    
+    @IBAction func whenShowGymsButtonPressed(_ sender: Any) {
+       
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Park"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        let search = MKLocalSearch(request: request)
+        
+        search.start { myResponse, myError in
+            guard let response = myResponse else { return }
+            for currentMapItem in response.mapItems {
+                self.gyms.append(currentMapItem)
+                let annotation = MKPointAnnotation()
+                annotation.title = currentMapItem.name
+                annotation.coordinate = currentMapItem.placemark.coordinate
+                self.mapView.addAnnotation(annotation)
+            }
+        }
+    }
+    
 }
