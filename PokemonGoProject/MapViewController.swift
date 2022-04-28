caecb7a1679e80455e56080fe0944443b2bcc158
Sam Yeh <syeh5413@stu.d214.org>
2022-04-28 14:13:26 -0500

    outlets and variables to set up MapKit. Also asks user for location

--- PokemonGoProject/MapViewController.swift
+++ PokemonGoProject/MapViewController.swift
@@ -6,13 +6,28 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var currentLocation:CLLocation!
+    var pokemon: [MKMapItem] = []
+    var gyms: [MKMapItem] = []
+    var initialRegion: MKCoordinateRegion!
+    var isInitialLoad = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
     }
     
 
