0b6e9146fff018bc747bb3b8dc7adb8b3371b5b4
Alex Hotca <ahotca2172@stu.d214.org>
2022-05-04 14:26:05 -0500

    Put in all new images into assists, finished the Pokédex view controller by putting all UILables, UIButtons, and UIImages.

--- PokemonGoProject/PokedexView.swift
+++ PokemonGoProject/PokedexView.swift
@@ -8,13 +8,11 @@
 import UIKit
 
 class PokedexView: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-
 }
+
