621e93e5ad4f82930c503157cf29cfc731bfdcbe
Alex Hotca <ahotca2172@stu.d214.org>
2022-05-10 14:12:28 -0500

    Fully finished the Pokédex view controller. Made initializer for Pokémon class.  Inputed data associated within each pokemon. Coded segue for each button when pressed.

--- PokemonGoProject/InfoViewController.swift
+++ PokemonGoProject/InfoViewController.swift
@@ -8,13 +8,32 @@
 import UIKit
 
 class InfoViewController: UIViewController {
-
+    
+    @IBOutlet weak var idNumber: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var pokeImage: UIImageView!
+    @IBOutlet weak var typeImageOne: UIImageView!
+    @IBOutlet weak var typeImageTwo: UIImageView!
+    @IBOutlet weak var CategoryLabel: UILabel!
+    @IBOutlet weak var HeightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        idNumber.text = pokemon.numberID
+        nameLabel.text = pokemon.name
+        pokeImage.image = pokemon.pictureName
+        typeImageOne.image = pokemon.type
+        typeImageTwo.image = pokemon.type2
+        CategoryLabel.text = pokemon.category
+        HeightLabel.text = pokemon.height
+        weightLabel.text = pokemon.weight
+        descriptionLabel.text = pokemon.description
+        
     }
-    
-
-   
 }
--- PokemonGoProject/PokemonClass.swift
+++ PokemonGoProject/PokemonClass.swift
@@ -6,17 +6,33 @@
 //
 
 import Foundation
+import UIKit
 
 class Pokemon {
     
-    var numberID = ""
-    var name = ""
-    var category = ""
-    var height = ""
-    var weight = ""
-    var description = ""
-    var pictureName = ""
-    var weakness = ""
-    var type = ""
-    var type2 = ""
+    var numberID:String
+    var name:String
+    var category:String
+    var height:String
+    var weight:String
+    var description:String
+    var pictureName:UIImage
+    var type:UIImage
+    var type2:UIImage
+    
+    init(numberID:String, name:String, category:String, height:String, weight:String, description:String, pictureName:String, type:String, type2:String) {
+        self.numberID = numberID
+        self.name = name
+        self.category = category
+        self.height = height
+        self.weight = weight
+        self.description = description
+        self.pictureName = UIImage(named: pictureName)!
+        self.type = UIImage(named: type)!
+        self.type2 = UIImage(named: type2)!
+        
+    }
+    
 }
+
+
